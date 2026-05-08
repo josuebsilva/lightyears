934a95d9b75a310a26836b7f8e73653f42617206
Josué <josue@tew.one>
2026-05-08 12:11:48 -0300

    Add SFML Library

--- LightYears/src/main.cpp
+++ LightYears/src/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main() {
 	std::cout << "Hello World" << std::endl;
+
+	sf::RenderWindow renderWindow{ sf::VideoMode({200, 200}), "My Window" };
 	return 0;
 }
